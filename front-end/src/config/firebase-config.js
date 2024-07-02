05c48983ad29f773435a759e0a947dc0d8422c22
xXTitouanXx <17titou@gmail.com>
2024-07-02 16:17:33 +0200

    config host

---
+++ front-end/src/config/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA7y9D83dMCLaqGft-fywkn_ub8vwkZcvk",
+    authDomain: "webstite-7c584.firebaseapp.com",
+    projectId: "webstite-7c584",
+    storageBucket: "webstite-7c584.appspot.com",
+    messagingSenderId: "334333610491",
+    appId: "1:334333610491:web:3a0a85858553472c6d235e"
+};
+
+ const app = initializeApp(firebaseConfig);
