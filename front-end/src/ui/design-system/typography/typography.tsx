f3068d9daf3aea71e09b7f2e8d12cfbf38e41a70
xXTitouanXx <17titou@gmail.com>
2024-07-04 13:54:05 +0200

    [ADD] correct typography.tsx

--- front-end/src/ui/design-system/typography/typography.tsx
+++ front-end/src/ui/design-system/typography/typography.tsx
@@ -17,7 +17,7 @@ interface TypographyProps {
         | "caption3"
         | "caption4";
     component?: "h1" | "h2" | "h3" | "h4" | "div" | "p" | "span";
-    theme?: "light" | "dark" | "black" | "gray" | "white";
+    theme?: "secondary" | "primary" | "black" | "gray" | "white";
     weight?: "regular" | "medium" | "semibold";
     className?: string;
     children: React.ReactNode;
@@ -31,15 +31,68 @@ export const Typography = ({
                                className,
                                children
                            }: TypographyProps) => {
-    let variantStyles: string = ""
+    let variantStyles: string = "", colorStyles: string = "";
     switch (variant) {
         case "display":
-            variantStyles = "text-red-500"
+            variantStyles = "text-8xl"
             break;
         case "h1":
-            variantStyles = "text-blue-500"
+            variantStyles = "text-7xl"
+            break;
+        case "h2":
+            variantStyles = "text-6xl"
+            break;
+        case "h3":
+            variantStyles = "text-5xl"
+            break;
+        case "h4":
+            variantStyles = "text-4xl"
+            break;
+        case "h5":
+            variantStyles = "text-3xl"
+            break;
+        case "lead":
+            variantStyles = "text-2xl"
+            break;
+        case "body-lg":
+            variantStyles = "text-lg"
+            break;
+        case "body-base":
+            variantStyles = "text-base"
+            break;
+       case "body-sm":
+            variantStyles = "text-sm"
+            break;
+        case "caption1":
+            variantStyles = "text-caption1"
+            break;
+         case "caption2":
+            variantStyles = "text-caption2"
+            break;
+         case "caption3":
+            variantStyles = "text-caption3"
+            break;
+         case "caption4":
+            variantStyles = "text-caption4"
             break;
     }
+    switch (theme) {
+        case "primary":
+            colorStyles="text-primary"
+            break
+        case "white":
+            colorStyles="text-white"
+            break
+        case "black":
+            colorStyles="text-gray"
+            break
+        case "gray":
+            colorStyles="text-gray-700"
+            break
+        case "secondary":
+            colorStyles="text-secondary"
+            break
+    }
     return <Component
-        className={clsx(variantStyles, weight === "medium" && "font-medium", className)}>{children}</Component>;
+        className={clsx(variantStyles,colorStyles, weight === "medium" && "font-medium", className)}>{children}</Component>;
 }
