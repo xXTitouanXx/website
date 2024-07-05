20c7688a315bdf8f695ce0f7ad7f64625e7f7e2a
xXTitouanXx <17titou@gmail.com>
2024-07-05 15:46:37 +0200

    [BUG] coding style

--- front-end/src/ui/design-system/call-to-action/call-to-action.view.tsx
+++ front-end/src/ui/design-system/call-to-action/call-to-action.view.tsx
@@ -13,7 +13,7 @@ export const CallToActionView = () => {
                         javhn dbfvoa bvpianvfa ...
                     </Typography>
                     <div>
-                        <Button variant="succes" baseUrl="/#" linkType={LinkTypes.EXTERNAL}>
+                        <Button variant="success" baseUrl="/#" linkType={LinkTypes.EXTERNAL}>
                             ovarbovaq
                         </Button>
                     </div>
